7dc740f01d10b603b1ab5e44fa0a89e78d044795
BuuHQ <BuuHQ@tpb.vn>
2023-09-19 16:58:24 +0700

    1st

---
+++ src/main/java/superheroes/backend/SuperheroesBackendApplication.java
@@ -0,0 +1,13 @@
+package superheroes.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperheroesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/superheroes/backend/SuperheroesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package superheroes.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
